24b6733ceffb829b9005686f7dc7b4aaed27fa28
basitng <basitng2004@gmail.com>
2024-02-06 16:05:09 +0100

    updated implementation

--- src/app/api/tts/route.ts
+++ src/app/api/tts/route.ts
@@ -1,19 +1,31 @@
 import { NextResponse } from "next/server";
-import { UnrealSpeechAPI, play, save } from "unrealspeech";
+import { UnrealSpeechAPI } from "unrealspeech";
 
-export async function POST(req: Request, res: Response) {
-  const { text, voice } = await req.json();
-
-  const unrealSpeech = new UnrealSpeechAPI(process.env.UNREAL_SPEECH_API_KEY!);
-  console.log(text, voice);
+export async function POST(req: Request) {
   try {
+    const { text, voice } = await req.json();
+
+    const unrealSpeech = new UnrealSpeechAPI(
+      process.env.UNREAL_SPEECH_API_KEY!
+    );
+
+    console.log(text, voice);
+
     const taskId = await unrealSpeech.createSynthesisTask(text, voice);
-    const status = await unrealSpeech.getSynthesisTaskStatus(taskId);
-    if (status.TaskStatus !== "completed") return;
-    console.log(status);
-    return NextResponse.json({ status });
+
+    while (true) {
+      const status = await unrealSpeech.getSynthesisTaskStatus(taskId);
+      if (status.TaskStatus === "completed") {
+        console.log(status);
+        return NextResponse.json({ status });
+      } else if (status.TaskStatus === "failed") {
+        throw new Error("Synthesis task failed.");
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error });
+    return NextResponse.json({ error: error || "An error occurred" });
   }
 }
